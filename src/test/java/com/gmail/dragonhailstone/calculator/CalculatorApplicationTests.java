6da3ad15bb76d92bbb330921fd5d6cee1015bbca
dragonhail <dragonhailstone@gmail.com>
2024-11-07 14:27:49 +0900

    code checkout and compile

---
+++ src/test/java/com/gmail/dragonhailstone/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.dragonhailstone.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
